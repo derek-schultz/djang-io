5381b5ceefb3934affde5b7ce492cc94f64ddd09
Derek Schultz <derek@quiet.af>
2016-02-28 22:51:02 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="djang-io",
-    version="0.2",
+    version="0.3",
     description="SocketIO integration for Django",
     author="Derek Schultz and D. P. Bulger",
     author_email='derek@quiet.af',
